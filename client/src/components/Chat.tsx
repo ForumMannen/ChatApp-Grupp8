f01078bf0ef2b6311e17d028108b7607b23b9fab
Carro <carolinaisabellaandersson@gmail.com>
2023-08-21 14:48:50 +0200

    Setup rooms

--- client/src/components/Chat.tsx
+++ client/src/components/Chat.tsx
@@ -1,14 +1,38 @@
+import { useState } from "react";
 import { useSocket } from "../context/socketContext";
-import RoomsList from "./RoomsList";
+// import RoomsList from "./RoomsList";
 
 function Chat() {
-  const { setRoom } = useSocket();
+  const { setRoom, rooms, createNewRoom, updatedRoomList } = useSocket();
+  const [newRoomName, setNewRoomName] = useState(""); // Här sparas värdet från inputfältet
+
   return (
     // Gör dynamiskt, lista.
     <div>
-      <RoomsList />
+      <input
+        type="text"
+        value={newRoomName}
+        onChange={(e) => setNewRoomName(e.target.value)}
+      />
+      <button onClick={() => createNewRoom(newRoomName)}>Gå med</button>
+
+      {updatedRoomList.map((room) => (
+        <li key={room.id}>{room.name} </li>
+      ))}
+      {/* {updatedRoomList.map((room) => (
+        <button key={room.id} onClick={() => setRoom(room.id)}>
+          Gå med i {room.name}
+        </button>
+      ))} */}
+
+      {rooms.map((room) => (
+        <button key={room.id} onClick={() => setRoom(room.id)}>
+          Gå med i {room.name}
+        </button>
+      ))}
+      {/* <RoomsList />
       <button onClick={() => setRoom("123")}>gå med i rum 123</button>
-      <button onClick={() => setRoom("456")}>Gå med i rum 456</button>
+      <button onClick={() => setRoom("456")}>Gå med i rum 456</button> */}
     </div>
   );
 }
--- client/src/context/socketContext.tsx
+++ client/src/context/socketContext.tsx
@@ -14,8 +14,11 @@ interface ISocketContext {
   setUsername: React.Dispatch<React.SetStateAction<string>>;
   room: string;
   setRoom: React.Dispatch<React.SetStateAction<string>>;
+  rooms: { id: string; name: string }[];
+  createNewRoom: (newRoomName: string) => void;
+  updatedRoomList: { id: string; name: string }[];
 }
-
+0;
 const defaultValues = {
   username: "",
   isLoggedIn: false,
@@ -23,6 +26,9 @@ const defaultValues = {
   setUsername: () => {},
   room: "",
   setRoom: () => {},
+  rooms: [],
+  createNewRoom: () => {},
+  updatedRoomList: [],
 };
 
 const SocketContext = createContext<ISocketContext>(defaultValues);
@@ -31,12 +37,17 @@ export const useSocket = () => useContext(SocketContext);
 const socket = io("http://localhost:3000", { autoConnect: false });
 
 const SocketProvider = ({ children }: PropsWithChildren) => {
-  //const roomsList = [{ id: "lobby", name: "lobby"}];
+  // const roomsList = [{ id: "lobby", name: "lobby"}];
   const [username, setUsername] = useState("");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [room, setRoom] = useState("");
-
-  // roomsList.push([...lobby, room])
+  const [updatedRoomList, setUpdatedRoomList] = useState([]);
+  //  roomsList.push(room);
+  const rooms = [
+    { id: "123", name: "Rum 123" },
+    { id: "456", name: "Rum 456" },
+    { id: "789", name: "Rum 789" },
+  ];
 
   useEffect(() => {
     if (room) {
@@ -49,9 +60,30 @@ const SocketProvider = ({ children }: PropsWithChildren) => {
     setIsLoggedIn(true);
     setRoom("lobby");
   };
+
+  const createNewRoom = (newRoomName: string) => {
+    const newRoom = { id: socket.id, name: newRoomName };
+    setRoom(newRoom); // Byt till det nya rummet
+    // rooms.push(newRoom); // Lägg till det nya rummet i listan
+    const updatedRooms = [...rooms, newRoom];
+    setUpdatedRoomList(updatedRooms);
+    console.log("Nytt rum skapat:", newRoom);
+    console.log("här är listan på rooms ", updatedRooms);
+  };
+
   return (
     <SocketContext.Provider
-      value={{ username, isLoggedIn, login, setUsername, room, setRoom }}
+      value={{
+        username,
+        isLoggedIn,
+        login,
+        setUsername,
+        room,
+        setRoom,
+        rooms,
+        createNewRoom,
+        updatedRoomList,
+      }}
     >
       {children}
     </SocketContext.Provider>
