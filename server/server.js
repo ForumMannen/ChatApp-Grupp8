ee3e23e28ff6ed84db59340408963d5d781d35eb
Carro <carolinaisabellaandersson@gmail.com>
2023-08-22 15:11:06 +0200

    solved roomlist
    Co-authored-by: Goodvibesmila <Goodvibesmila@users.noreply.github.com>
    Co-authored-by: Linus Lundell <lundell.linus@gmail.com>

--- server/server.js
+++ server/server.js
@@ -16,11 +16,16 @@ app.use(cors());
 
 io.on("connection", (socket) => {
   console.log("New user connected: ", socket.id);
+  // socket.emit("New_room_is_added_to_list", socket);
 
   socket.on("join_room", (room) => {
     socket.join(room);
     console.log(io.sockets.adapter.rooms);
   });
+
+  // socket.on("create_new_room", (roomList) => {
+  //   io.emit("New_room_is_added_to_list", roomList);
+  // });
 });
 
 server.listen(3000, () => console.log("Servern är igång"));
